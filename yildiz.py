fd6a49cc09e648a5b2a4a48583107f04eb702b85
Semih Ocaklı <semih.ocakli@ogr.ksbu.edu.tr>
2024-12-26 09:51:48 +0300

    mesaj

--- yildiz.py
+++ yildiz.py
@@ -1,7 +1,6 @@
-def yildiz_olustur(n):
-    return "\n".join("*" * i for i in range(1, n + 1))
 
-# def yildiz_olustur(n):
-#     if n == 0:
-#         return ""
-#     return yildiz_olustur(n - 1) + ("\n" if n > 1 else "") + "*" * n
+
+def yildiz_olustur(n):
+    if n == 0:
+        return ""
+    return yildiz_olustur(n - 1) + ("\n" if n > 1 else "") + "*" * n
